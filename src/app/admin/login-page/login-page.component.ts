2c3390e69e7c3bc5f6681b9556214513ab0ef25b
Kathreen Fenchenko <fev3@mail.ru>
2020-08-06 19:17:24 +0300

    auth added

---
+++ src/app/admin/login-page/login-page.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {User} from '../shared/interfaces'
+import { AuthService } from '../shared/services/auth.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit {
+  submitted = false
+  form: FormGroup
+  message: string
+  constructor(
+    public auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe( (params: Params) => {
+      if (params['loginAgain']) {
+        this.message = 'Пожалуйста введите данные'
+      }
+    })
+
+    this.form = new FormGroup(
+      {
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+      }
+    )
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
+    this.submitted = true
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+      returnSecureToken: true
+    }
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset()
+      this.router.navigate(['/admin', 'dashboard'])
+      this.submitted = false
+    }, () => {
+      this.submitted = false
+    } )
+  }
+
+}
---
+++ src/app/admin/shared/services/auth,guard.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core'
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(
+        private auth: AuthService,
+        private router: Router) {}
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot
+        ): boolean | import("rxjs").Observable<boolean> |  Promise<boolean> {
+        if (this.auth.isAuthenticated())  {
+            return true
+        }  else {
+            this.auth.logout()
+            this.router.navigate(['/admin', 'login'], {queryParams: {
+                loginAgain: true
+            }})
+        }
+            
+        
+       // throw new Error("Method not implemented.");
+    }
+
+}
